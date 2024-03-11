7e1d824d5ca78697690079961e428658a8ba318d
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-03-11 11:27:53 +0300

    Core katmanı içerisine Application Kütüphanesi eklendi ve Interfaces klasörü içerisine IRepository sınıfı yazıldı

---
+++ Core/UdemyCarBook.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace UdemyCarBook.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetListAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
