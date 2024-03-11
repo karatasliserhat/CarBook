776c7ba8ad993bdb7f12a2f1033d7d68286e8c15
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2024-03-11 11:41:17 +0300

    Presentation kütüphanesinin içerisine Repository sınıfı yazıldı Application katmanındaki interface sınıfı miras alındı test ok

--- Core/UdemyCarBook.Application/Interfaces/IRepository.cs
+++ Core/UdemyCarBook.Application/Interfaces/IRepository.cs
@@ -6,6 +6,6 @@
         Task<T> GetByIdAsync(int id);
         Task<T> CreateAsync(T entity);
         Task UpdateAsync(T entity);
-        Task RemoveAsync(T entity);
+        Task DeleteAsync(T entity);
     }
 }
